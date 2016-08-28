78645ae8d76672ef34b16471d24222165abdaf94
feuzeu <thierry.feuzeu@gmail.com>
2016-08-28 01:45:57 -0400

    Updated and tested the module.

--- src/Controller.php
+++ src/Controller.php
@@ -2,51 +2,6 @@
 
 namespace Jaxon\Zend;
 
-class Controller
+class Controller extends \Jaxon\Framework\Controller
 {
-    use \Jaxon\Request\FactoryTrait;
-
-    // Application data
-    // These data will be set by the Zend Jaxon library when registering the controller
-    public $view = null;
-    public $zfJaxon = null; // The Zend Jaxon module
-    public $response = null;
-
-    /**
-     * Create a new Controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {}
-
-    /**
-     * Initialise the controller.
-     *
-     * @return void
-     */
-    public function init()
-    {}
-
-    /**
-     * Find an Jaxon controller by name
-     *
-     * @param string $method the name of the method
-     * 
-     * @return object the Jaxon controller, or null
-     */
-    public function controller($name)
-    {
-        // If the class name starts with a dot, then find the class in the same class path as the caller
-        if(substr($name, 0, 1) == '.')
-        {
-            $name = $this->getJaxonClassPath() . $name;
-        }
-        // The controller namespace is prepended to the class name
-        else if(($namespace = $this->getJaxonNamespace()))
-        {
-            $name = str_replace(array('\\'), array('.'), trim($namespace, '\\')) . '.' . $name;
-        }
-        return (($this->zfJaxon) ? $this->zfJaxon->controller($name) : null);
-    }
 }
