64f2c0cd74789b5415bf41f34cfabb8f74ffb8ab
feuzeu <thierry.feuzeu@gmail.com>
2016-10-15 20:45:18 -0400

    Update following an update to the config in the jaxon-core package.

--- src/Controller/Plugin/Jaxon.php
+++ src/Controller/Plugin/Jaxon.php
@@ -32,14 +32,28 @@ class Jaxon extends AbstractPlugin
         $this->view = new \Jaxon\Zend\View();
 
         $appPath = rtrim(getcwd(), '/');
-        $config = require($appPath . '/config/jaxon.config.php');
-        $appConfig = array_key_exists('app', $config) ? $config['app'] : array();
-        $libConfig = array_key_exists('lib', $config) ? $config['lib'] : array();
+        $debug = true;
+
+        // Use the Composer autoloader
+        $this->jaxon->useComposerAutoloader();
+        // Jaxon library default options
+        $this->jaxon->setOptions(array(
+            'js.app.extern' => !$debug,
+            'js.app.minify' => !$debug,
+            'js.app.uri' => $this->baseUrl . '/jaxon/js',
+            'js.app.dir' => $this->baseDir . '/jaxon/js',
+        ));
+        // Jaxon library settings
+        $config = $this->jaxon->readConfigFile($appPath . '/config/jaxon.config.php', 'lib');
 
         // Jaxon application settings
+        $appConfig = array();
+        if(array_key_exists('app', $config) && is_array($config['app']))
+        {
+            $appConfig = $config['app'];
+        }
         $controllerDir = (array_key_exists('dir', $appConfig) ? $appConfig['dir'] : $appPath . '/jaxon');
         $namespace = (array_key_exists('namespace', $appConfig) ? $appConfig['namespace'] : '\\Jaxon\\App');
-
         $excluded = (array_key_exists('excluded', $appConfig) ? $appConfig['excluded'] : array());
         // The public methods of the Controller base class must not be exported to javascript
         $controllerClass = new \ReflectionClass('\\Jaxon\\Zend\\Controller');
@@ -47,15 +61,7 @@ class Jaxon extends AbstractPlugin
         {
             $excluded[] = $xMethod->getShortName();
         }
-        // Use the Composer autoloader
-        $this->jaxon->useComposerAutoloader();
-        // Jaxon library default options
-        $this->jaxon->setOptions(array(
-            'js.app.extern' => false,
-            'js.app.minify' => false,
-        ));
-        // Jaxon library settings
-        \Jaxon\Config\Config::setOptions($libConfig);
+
         // Set the request URI
         if(!$this->jaxon->getOption('core.request.uri'))
         {
